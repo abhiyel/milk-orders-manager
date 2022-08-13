83c05190002d4a13c881c71e70be36536e012881
AbhishekYelne <abhiyel12@gmail.com>
2022-08-13 16:42:47 +0530

    Made changes to print max capacity

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ const orderRouter = require("./routers/order");
 
 const app = express();
 const port = process.env.PORT;
+const maxCapacity = process.env.MAX_CAPACITY;
 
 app.use(express.json());
 app.use(orderRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
+  console.log(
+    "Maximum capacity of milk per day is fixed to " + maxCapacity + " Litres."
+  );
 });
